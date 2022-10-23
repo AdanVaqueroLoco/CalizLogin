f8063dbb6b64ae89d4eff3aec32356a86137c835
AdanVaqueroLoco <adan905@hotmail.com>
2022-10-22 23:55:38 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { config } from './config';
+import { PublicClientApplication } from '@azure/msal-browser';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isAuthenticated: false,
+      user: ()
+    };
+    this.login = this.login.bind(this)
+    // Initialize the MSAL application object
+    this.PublicClientApplication = new PublicClientApplication({
+      auth: {
+        clientId: config.appid,
+        redirectUri: config.redirectUrl,
+        authority:config.authority
+      },
+      cache: {
+        cacheLocation: "sessionStorage",
+        storeAuthStateInCookie: true
+      }
+    });
+  }
+  async login(){
+    try {
+      // Login via popup
+      await this.PublicClientApplication.loginPopup(
+        {
+          scopes: config.scopes,
+          prompt: "select_account"
+        }
+      )
+    }
+  }
 
 export default App;
---
+++ src/Config.js
@@ -0,0 +1,8 @@
+export const config = {
+    appid: "67328865-aa9d-473e-b3f9-f6640d2224fb",
+    redirectUrl: "http://localhost:3000",
+    scopes: [
+        'user.read'
+    ],
+    authority: 'https://login.microsoftonline.com/adan905hotmail.onmicrosoft.com'
+};
